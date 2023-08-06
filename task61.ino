242876d93f2324f55a57cab7a3b3d27273c508ea
Yossefelhamoley <138775003+Yossefelhamoley@users.noreply.github.com>
2023-08-07 02:52:49 +0300

    Add files via upload

---
+++ task61.ino
@@ -0,0 +1,27 @@
+int tilt = 2;
+int led = 3; 
+
+
+void setup()
+{
+  pinMode(tilt, INPUT);
+  pinMode(led, OUTPUT);
+  
+}
+ 
+void loop()
+{
+  int reading;
+  reading = digitalRead(tilt);
+  
+  if(reading)
+  {
+    digitalWrite(led, LOW);
+    
+  }
+  else
+  {
+    digitalWrite(led, HIGH);
+    
+  }
+}
